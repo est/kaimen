705d6bc5f6ff55a5bdc16e36c9d906d4d1c0ac77
est <electronicstar@126.com>
2017-02-21 20:57:04 +0800

    init commit

---
+++ kaimen.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
---
+++ readme.md
@@ -0,0 +1,6 @@
+KaiMen - port knocking in Python
+
+
+## References
+
+http://www.portknocking.org/view/implementations
